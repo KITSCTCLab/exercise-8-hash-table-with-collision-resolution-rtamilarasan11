76cbb7cf2494c0015df6231d594b7904fac48a67
rtamilarasan11 <109951923+rtamilarasan11@users.noreply.github.com>
2022-10-03 12:18:16 +0530

    6th

--- Main.py
+++ Main.py
@@ -1,1 +1,32 @@
+import re
 
+def display_hash(hashtable) -> None:
+	for keyvalue in range(len(hashtable)):
+		cont = hashtable[keyvalue]
+		print(keyvalue, end = " ")
+		for element in cont:
+			print("-->", element, end = " ")
+		print()
+
+def Hashing(keyvalue) -> int:
+	return keyvalue % len(HashTable)
+
+def insert(Hashtable, keyvalue, value) -> None:
+	# Write your code here
+	Hashtable[Hashing(keyvalue)].append(value)
+
+
+
+# Do not edit the following code
+hash_table_size = int(input())
+# Create Hashtable as a list of list.
+HashTable = [[] for _ in range(hash_table_size)]
+input_data = input()
+data = []
+for item in re.split('], |].', input_data):
+  item = item[1:]
+  data = item.split(', ')
+  if len(data) > 1:
+    insert(HashTable, int(data[0]), data[1])
+
+display_hash (HashTable)
